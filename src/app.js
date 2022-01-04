e241e560141a2e6710503c88030bdabc6e787bd5
onkar-date <onkar101197@gmail.com>
2022-01-04 11:58:33 +0530

    .env file removed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-
-require('dotenv').config({ path: '../.env'});
 const express = require('express');
 const cors = require('cors');
 const app = express();
--- src/utilities/connection.js
+++ src/utilities/connection.js
@@ -1,7 +1,6 @@
-require('dotenv').config({ path: '../.env'});
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const dbUrl = process.env.DB_CONNECTION_STRING;
+const dbUrl = "mongodb://wanderlustAdmin:wanderlust1234@cluster0-shard-00-00.bridr.mongodb.net:27017,cluster0-shard-00-01.bridr.mongodb.net:27017,cluster0-shard-00-02.bridr.mongodb.net:27017/wanderlustDB?ssl=true&replicaSet=atlas-10ngx2-shard-0&authSource=admin&retryWrites=true&w=majority";
 module.exports = function() {
     // Connection to mongoDB Atlas
     mongoose.connect(
